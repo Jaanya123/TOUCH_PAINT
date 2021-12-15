c6eb7b1c69f38821fdf98f147a067f720e8279db
Jaanya123 <90129064+Jaanya123@users.noreply.github.com>
2021-12-15 14:01:13 +0530

    Add files via upload

---
+++ PAINT.js
@@ -0,0 +1,12 @@
+var mouseEvent="empty"
+var last_position_of_x, last_position_of_y;
+
+canvas= document.getElementById('myCanvas');
+ctx= canvas.getContext("2d");
+
+color:"black";
+width_of_line= 2;
+
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{}
